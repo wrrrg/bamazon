f85c2549f69524952dae084fb853e72da5514137
wrrrg <bill.gryta@gmail.com>
2018-02-28 14:31:37 -0600

    removed departments

--- initialSchema.sql
+++ initialSchema.sql
@@ -6,7 +6,6 @@ USE bamazon;
 CREATE TABLE products(
 	id int not null auto_increment,
 	product_name varchar(50) not null,
-	department_name varchar(50) not null,
 	price int default 0,
 	stock_quantity int default 0,
   	PRIMARY KEY (id)
--- product_seeds.sql
+++ product_seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Shirt", "Clothing", 30, 5),
-		 ("Pants", "Clothing", 20, 3),
-		 ("Laptop", "Tech", 200, 7),
-		 ("Phone", "Tech", 150, 15),
-		 ("Tablet", "Tech", 125, 2),
-		 ("Fancy Japanese Sunscreen", "Beauty", 30, 8),
-		 ("Face Wash", "Beauty", 15, 4),
-		 ("My System", "Books", 8, 13),
-		 ("Logical Chess Move by Move", "Books", 9, 14),
-		 ("Reasses Your Chess", "Books", 13, 2);
+INSERT INTO products (product_name, price, stock_quantity)
+VALUES ("Shirt",  30, 5),
+		 ("Pants",  20, 3),
+		 ("Laptop",  200, 7),
+		 ("Phone", 150, 15),
+		 ("Tablet", 125, 2),
+		 ("Fancy Japanese Sunscreen", 30, 8),
+		 ("Face Wash", 15, 4),
+		 ("My System", 8, 13),
+		 ("Logical Chess Move by Move", 9, 14),
+		 ("Reasses Your Chess", 13, 2);
